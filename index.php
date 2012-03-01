b389650ae82530ced79eeb6111b92fa5a7a57372
Flavio Vianello <fvianello@h-art.it>
2012-03-01 18:21:21 +0100

    test

--- index.php
+++ index.php
@@ -1,55 +1,9 @@
-<?php
-require_once("./vendor/.composer/autoload.php");
-//require_once("./lib/FBSignedRequest.php");
+<?php require_once __DIR__.'/silex.phar'; 
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+$app = new Silex\Application(); 
 
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Response;
+$app->get('/hello/{name}', function($name) use($app) { 
+    return 'Hello '.$app->escape($name); 
+}); 
 
-$app = new Silex\Application();
-
-//$sr = new FBSignedRequest($_REQUEST, '2062f6a24e3a60d3521dca7078192bfc');
-
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-  'twig.path'       => __DIR__.'/views',
-  'twig.class_path' => __DIR__.'/vendor/twig/lib',
-));
-
-$app->match('/page/{slug}', function (Application $app, $slug) {
-  $template_name='pages/'.$app->escape($slug).'.twig';
-  if (file_exists(__DIR__.'/views/'.$template_name)) {
-  return $app['twig']->render($template_name, array(
-    'slug' => $slug,
-  ));
-  } else {
-  $message = "Template ".$app->escape($slug)." not exists";
-  return new Symfony\Component\HttpFoundation\Response($message, 404);
-  }
-});
-
-$app->match('/', function (Application $app) {
-  $template_name = "index.twig";
-  return $app['twig']->render($template_name, array(
-  ));
-});
-
-
-$app->error(function (\Exception $e, $code) use ($app) {
-  switch ($code) {
-    case 404:
-      $message = 'The requested page could not be found.';
-      $template_name = "errors/404.twig";
-      return $app['twig']->render($template_name, array(
-      ));
-      break;
-    default:
-      $message = 'We are sorry, but something went terribly wrong.'.$e->getMessage();
-  }
-
-  return new Response($message, $code);
-});
-$app['debug'] = true;
-
-$app->run();
+$app->run(); 
